e3dffe27ff4155e81b54e3c4073d164c5784963c
AbdAlRahmanGad <abdo.gta22@gmail.com>
2023-07-21 15:41:20 +0300

    Finished the get command

--- main.c
+++ main.c
@@ -6,6 +6,10 @@
 
 void printAll(FILE *pFile);
 
+void badCommand();
+
+void getV(FILE *pFile, char *k);
+
 /* cat:  concatenate files, version 1 */
 int main(int argc, char *argv[])
 {
@@ -21,6 +25,7 @@ int main(int argc, char *argv[])
 //    s."fff";
 //    char *s;
     char *k = argv[1];
+//    bool validCommad =
     if(k[0] == 'p'){
         char *s;
         int x = 0;
@@ -35,75 +40,8 @@ int main(int argc, char *argv[])
         fputs( "\n",fp);
 
     }else if(k[0] == 'g'){
-        char *s;
-
-        int x =0;
-        char* key;
-        int exp = 1;
-        while ((s = strsep(&k, ",")) != NULL ){
-            if(x!=0) {
-              key = s;
-                for (int i = 0; i < strlen(s); ++i) {
-                   if( isdigit(s[i]) == 0){
-                       ///// exception
-                       exp = 0;
-                       break;
-                   }
-                }
-                //                      (int) strtol(s, (char **)NULL, 10);
-            }x++;
-        }
-        if(exp==0){
-            printf("%s","not number");
-        }else{
-            char *s;
-
-            char * line = NULL;
-            size_t len = 0;
-//        printf("%s","comp_____");
-
-            fp = fopen("database.txt", "r");
-            if (fp == NULL){
-                exit(EXIT_FAILURE);
-            }
-//            char* comp;
-            int cnt = 0;
-//            while ((s = strsep(&k, ",")) != NULL ){
-//                if(cnt == 1) {
-//                    comp = s;
-//                }else cnt++;
-//            }
-            bool finished = false;
-            while ((getline(&line, &len, fp)) != -1) {
-//            printf("%s", line);
-//            for (int i = 0; i < strlen(line); ++i) {
-                int x = 0;
-                while ((s = strsep(&line, ",")) != NULL ){
-                    if(x==1){
-                        printf("%s",s);
-                        finished = true;
-                        break;
-                    }
-                    if(strcmp(s,key)==0){
-                        printf("%s,",s);
-
-                        x++;
-                    }else{
-                        break;
-                    }
-                }
-                if(finished)break;
-                else{
-                    printf("%s", "not found");
-                    break;
-                }
-//            }
-//            remove(line);
-        }
-//        fgets(fp);
-//        printf("%s",key);
-
-    }}else if(k[0] == 'd'){
+        getV(fp,k);
+    }else if(k[0] == 'd'){
         char *s;
 
         char * line = NULL;
@@ -156,7 +94,7 @@ int main(int argc, char *argv[])
     else if(strcmp(k , "a") == 0){
         printAll(fp);
     }else{
-        printf("%s" , "bad command");
+        badCommand();
     }
 //    fwrite();
 //    fputs( argv[1],fp);
@@ -168,6 +106,85 @@ int main(int argc, char *argv[])
      fclose(fp);
     return 0;
 }
+
+void getV(FILE *fp, char *k) {
+    char *s;
+    int x =0;
+    char* key;
+    int exp = 1;
+    int comma=0;
+    for (int i = 0; i < strlen(k); ++i) {
+//        printf("%c%s",k[i],"_");
+
+        if(k[i]==','){
+            comma++;
+        }
+    }
+    if(comma!=1){
+        exp = 0;
+//            break;
+    }
+    while ((s = strsep(&k, ",")) != NULL ){
+        if(x>1){
+            exp = 0;
+            break;
+        }
+        if(x == 0 ){
+            if(strcmp("g",s)!=0){
+                exp = 0;
+                break;
+            }
+        }
+        if(x==1) {
+            key = s;
+            for (int i = 0; i < strlen(s); ++i) {
+                if( isdigit(s[i]) == 0){
+                    ///// exception
+                    exp = 0;
+                    break;
+                }
+            }
+        }x++;
+    }
+    if(exp==0){
+        badCommand();
+    }else{
+        char *s;
+        char * line = NULL;
+        size_t len = 0;
+        fp = fopen("database.txt", "r");
+        if (fp == NULL){
+            exit(EXIT_FAILURE);
+        }
+        bool finished = false;
+        while ((getline(&line, &len, fp)) != -1) {
+            int x = 0;
+            while ((s = strsep(&line, ",")) != NULL ){
+                if(x==1){
+                    printf("%s",s);
+                    finished = true;
+                    break;
+                }
+                if(strcmp(s,key)==0){
+                    printf("%s,",s);
+
+                    x++;
+                }else{
+                    break;
+                }
+            }
+            if(finished){
+                break;
+            }
+        }
+        if(!finished){
+            printf("%s%s",key," not found");
+        }
+
+    }
+}
+
+void badCommand() { printf("%s" , "bad command"); }
 
 void printAll(FILE *fp) {
     char * line = NULL;
@@ -181,7 +198,10 @@ void printAll(FILE *fp) {
         printf("%s", line);
     }
 }
-
+//void getV(FILE *fp , char*k)
+//{
+//
+//}
 /* filecopy:  copy file ifp to file ofp */
 //void filecopy(FILE *ifp, FILE *ofp)
 //{  int c;
